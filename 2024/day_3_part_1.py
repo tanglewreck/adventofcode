e0c2540795fbbeee79bc46bd47a94aef7b04b4ee
mier (betelgeuse) <mier@tanglewreck.mier.org>
2025-05-24 21:13:46 +0200

    day_3_part_1.py: Rewrote the parser-code (better, beautifuler). Now able to recognise 'mul()' tokens as well as 'do()' and 'don't()' tokens.

--- 2024/day_3_part_1.py
+++ 2024/day_3_part_1.py
@@ -33,8 +33,40 @@
     if you add up all of the results of the multiplications?
 
     ------------------------------------------------------------
-
+    Your puzzle answer was 188192787.
+    The first half of this puzzle is complete! It provides one gold star: *
     ------------------------------------------------------------
+
+
+    --- Part Two ---
+
+    As you scan through the corrupted memory, you notice that some of the
+    conditional statements are also still intact. If you handle some of the
+    uncorrupted conditional statements in the program, you might be able to get an
+    even more accurate result.
+
+    There are two new instructions you'll need to handle:
+
+        - The do() instruction enables future mul instructions.
+        - The don't() instruction disables future mul instructions.
+
+    Only the most recent do() or don't() instruction applies. At the beginning of
+    the program, mul instructions are enabled.
+
+    For example:
+
+    xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))
+
+    This corrupted memory is similar to the example from before, but this time the
+    mul(5,5) and mul(11,8) instructions are disabled because there is a don't()
+    instruction before them. The other mul instructions function normally,
+    including the one at the end that gets re-enabled by a do() instruction.
+
+    This time, the sum of the results is 48 (2*4 + 8*5).
+
+    Handle the new instructions; what do you get if you add up all of the results
+    of just the enabled multiplications?
+
 """
 
 
@@ -42,49 +74,185 @@
 # import numpy as np
 # import pandas as pd
 # pylint: enable=unused-import
-import re
+# import re
+from string import digits
 
 
-def mul(x: int|float, y: int|float) -> int | float:
+# def mul(x: int|float, y: int|float) -> int | float:
+#     """Multiply two numbers"""
+#     return x * y
+#
+#
+# def day_3_part_1() -> None:
+#     """day_3_part_1()"""
+#     fdata = "data/day_3_data"
+#     fdata_example = "data/day_3_data_example"
+#     # Import data
+#     try:
+#         with open(fdata_example, encoding="utf-8") as fp:
+#             data_example = fp.read()
+#         with open(fdata, encoding="utf-8") as fp:
+#             data= fp.read()
+#     except OSError as exception:
+#         print(repr(exception))
+#         raise SystemExit(1) from exception
+#     # Compile a regexp that will match 'mul(<num>,<num>)'
+#     mul_regexp = re.compile(r"mul\(\d+,\s*\d+\)")
+#     # Prepare two lists that will hold the reults of
+#     # the multiplications
+#     mul_results, mul_results_example = [], []
+#     # Get results for the example-data
+#     for match in mul_regexp.findall(data_example):
+#         # pylint: disable=eval-used
+#         mul_results_example.append(eval(match))
+#     # Get results for the full data
+#     for match in mul_regexp.findall(data):
+#         # pylint: disable=eval-used
+#         mul_results.append(eval(match))
+#     # Print results
+#     print(f"Sum of multiplications (example data)= {sum(mul_results_example)}")
+#     print(f"Sum of multiplications (full data) = {sum(mul_results)}")
+#
+#
+
+# TEST_DATA = "xyxmul(123,456)xyzmuöl(38),.mul(1,2)mul(3,3333)sjsjX"
+TEST_DATA = "xyxmul(100,456)xyzmuöl(38),.mul(1,2)mul(3,10)sjsjX"
+TEST_DATA_2 = "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))"
+
+
+def mul(x: int|float, y: int|float) -> int|float:
     """Multiply two numbers"""
     return x * y
 
 
-def day_3_part_1() -> None:
-    """day_3_part_1()"""
+def get_token(data_string: str, index: int, length: int) -> str:
+    """
+        get_token()
+            Return a substring of length 'length' beginning at 'index'.
+    """
+    try:
+        return data_string[index : index + length]
+    except IndexError:
+        return ""
+
+# pylint: disable=too-many-branches
+def parse_data(data: str = TEST_DATA, do_dont: bool = False,
+               verbose: int = 0) -> list:
+    """
+        parse_data()
+            Extracts valid tokens (substrings of the form 'mul(x,y)', where x and y
+            are integers) from a string.
+
+            Returns a list of token-strings.
+    """
+    valid_tokens = []  # A list that will hold valid tokens ('mul(x,y)'-substrings)
+    valid = True  # Indicator that a token is (still) valid
+    eof = False  # flag that indicates whether we have reached end-of-file (end-of-data)
+    index = 0  # index into to the data
+    # do_token_length = 4  # length of a 'do()' token
+    # dont_token_length = 7  # length of a "don't()" token
+    do = True
+    while valid and not eof:
+        try:
+            # Check for 'do()' and "don't()"tokens
+            if get_token(data, index, 4) == "do()":
+                if verbose > 0:
+                    print("found a 'do()' token")
+                do = True
+            if get_token(data, index, 7) == "don't()":
+                if verbose > 0:
+                    print("found a 'don't()' token")
+                do = False
+            # Set token-length
+            token_length = 4
+            # Check that we do not go beoynd the length of the data
+            if index + token_length > len(data):
+                eof = True
+                continue
+            # Get next token
+            token = get_token(data, index, token_length)
+            # If token does not match the beginning of a 'mul(x,y)' operation
+            # move one step forward and start the while-loop over
+            if token != "mul(":
+                index += 1
+                continue
+            # Increase token-length by 1
+            token_length += 1
+            # Loop while the token ends in a digit
+            while get_token(data, index, token_length)[-1] in digits:
+                token = get_token(data, index, token_length)
+                token_length += 1
+            # If the token now ends with a ",", update the token
+            # and investigate further
+            if get_token(data, index, token_length)[-1] == ",":
+                token = get_token(data, index, token_length)
+                token_length += 1
+                # Again, if the token ends with a digit, increase token-length
+                # by one, update the token, and and repeat until the token
+                # does not end with a digit
+                while get_token(data, index, token_length)[-1] in digits:
+                    token = get_token(data, index, token_length)
+                    token_length += 1
+                # Token now ends with a non-digit. If it ends with a ")",
+                # we have found a valid token, so we add it to the list of
+                # valid tokens, but only if the lastest do()/don't() token
+                # was a 'do()'.
+                if get_token(data, index, token_length)[-1] == ")":
+                    token = get_token(data, index, token_length)
+                    if do or not do_dont:
+                        valid_tokens.append(token)
+                    token_length += 1
+                    if verbose > 0:
+                        print(token)
+            index += len(token)
+        except IndexError:
+            pass
+    if verbose > 0:
+        print(valid_tokens)
+    return valid_tokens
+
+
+def main() -> None:
+    """main()"""
+# pylint: disable=unused-variable
+    # Import data
     fdata = "data/day_3_data"
     fdata_example = "data/day_3_data_example"
-    # Import data
+    fdata_example_2 = "data/day_3_data_example_part_2"
     try:
         with open(fdata_example, encoding="utf-8") as fp:
-            data_example = fp.read()
+            example_data = fp.read()
+        with open(fdata_example_2, encoding="utf-8") as fp:
+            example_data_2 = fp.read()
         with open(fdata, encoding="utf-8") as fp:
-            data= fp.read()
+            full_data= fp.read()
+        data = example_data_2
+# pylint: enable=unused-variable
     except OSError as exception:
         print(repr(exception))
         raise SystemExit(1) from exception
-    # Compile a regexp that will match 'mul(<num>,<num>)'
-    mul_regexp = re.compile(r"mul\(\d+,\s*\d+\)")
-    # Prepare two lists that will hold the reults of
-    # the multiplications
-    mul_results, mul_results_example = [], []
-    # Get results for the example-data
-    for match in mul_regexp.findall(data_example):
-        # pylint: disable=eval-used
-        mul_results_example.append(eval(match))
-    # Get results for the full data
-    for match in mul_regexp.findall(data):
-        # pylint: disable=eval-used
-        mul_results.append(eval(match))
-    # Print results
-    print(f"Sum of multiplications (example data)= {sum(mul_results_example)}")
-    print(f"Sum of multiplications (full data) = {sum(mul_results)}")
+
+    # Get tokens
+    tokens = parse_data(data, do_dont=False, verbose=0)
+    token_sum = 0
+    # pylint: disable=eval-used
+    for token in tokens:
+        token_sum += eval(token)
+    print("do/dont = False")
+    print(f"Sum of mul()-tokens = {token_sum}")
+    print(f"Number of mul()-tokens = {len(tokens)}")
+    print()
 
 
-def main():
-    """main() - a redundant level of indirection..."""
-    day_3_part_1()
-
+    # Get tokens
+    tokens = parse_data(data, do_dont=True, verbose=0)
+    token_sum = 0
+    # pylint: disable=eval-used
+    for token in tokens:
+        token_sum += eval(token)
+    print("do/dont = True")
+    print(f"Sum of mul()-tokens = {token_sum}")
+    print(f"Number of mul()-tokens = {len(tokens)}")
 
 if __name__ == "__main__":
     main()
