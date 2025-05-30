a4b2f677b667994b011efbf90ae75e5eeb3af2a9
mier (betelgeuse) <mier@tanglewreck.mier.org>
2025-05-30 16:15:44 +0200

    snippets/day_4_4_diags.py: rewrote and added (overly complicated) functions for extracting diagonals; still gives the wrong answer, though, but only for the full data, not for the example data, which is kind of weird

--- 2024/snippets/day_4_4_diags.py
+++ 2024/snippets/day_4_4_diags.py
@@ -12,26 +12,65 @@ __all__ = ["lower_left_diagonals",
            "extract_diagonals",
            "DF", "N"]
 __author__ = "mier"
-__name__ = "snippets.day_4_4_diags"  # pylint: disable=redefined-bultin
 __version__ = "0.4.4"
 
 
+# pylint: disable=unused-import
+from typing import Any, Optional, TYPE_CHECKING
 import numpy as np
 import pandas as pd
 
-DF = pd.read_csv("data/day_4_data_example.csv", header=None)
-N = len(DF)
+DF = None
+N = 10
+try:
+    DF = pd.read_csv("data/day_4_data_example.csv", header=None)
+    N = len(DF)
+except pd.errors.EmptyDataError as exception:
+    print(f"WARNING: {repr(exception)}")
 
 
-def lower_left_diagonals(df: pd.DataFrame = DF,
+def lower_left_diagonals(df = DF,
+                         start: int | None = N - 4,
+                         stop: int | None = -1) -> list:  # mypy: no-operator
+    """
+        lower_left_diagonals():
+            Extract diagonals starting at the
+            lower-right corner and going down-left;
+            i.e. start in the lower-right corner, moving up,
+            extract diagonals starting at (k, n - 1),
+            for k = (n-1), (n-1), ... , 0.
+
+        CORRECT
+
+    """
+    d = np.asarray(df)
+    # Flip the array left-right so that we
+    # can use np.diagonal().
+    dflip = np.fliplr(d)
+    n = len(df)  # mypy: no-arg-type
+    # Amend start position (row)
+    if start == -1:
+        start = n - 4
+    diags = []
+    for k in range(start, stop, -1):
+        diag = dflip[k:n, 0:(n - k)].diagonal()
+        diags.append(diag)
+    return diags
+
+
+def lower_right_diagonals(df = DF,
                          start: int = -1,
                          stop: int = -1) -> list:
     """
-        lower_left_diagonals()
-            Extract lower-left diagonals going down-right,
+        lower_right_diagonals()
+            Extract diagonals starting at the
+            lower-left corner and going down-right,
             i.e. start in the lower-left corner, moving up,
             extract diagonals starting at (k, 0),
             for k = (n-1), (n-1), ... , 0.
+
+    CORRECT!
+
     """
     # Convert to np.ndarray so we can use np.diagonal()
     d = np.asarray(df)
@@ -40,22 +79,25 @@ def lower_left_diagonals(df: pd.DataFrame = DF,
     # lower-left corner,i.e. at (0, n - 1), but since
     # we will be looking for the string 'XMAS' there's
     # no use in partitioning the data into smaller chunks
-    # than 4 by 4. In that case, start at (0, n - 4).
+    # than 4 by 4. So, unless explicitly being told otherwise,
+    # start at (n - 4, 0), extracting a 4 x 4 partion.
+    # Amend start position (row)
     if start == -1:
         start = n - 4
     diags = []
     for row in range(start, stop, -1):
-        diag = d[row:n, :n - row].diagonal()
+        diag = d[row:n, 0:(n - row)].diagonal()
         diags.append(diag)
     return diags
 
 
-def upper_right_diagonals(df: pd.DataFrame = DF,
+def upper_right_diagonals(df = DF,
                           start: int = -1,
                           stop: int = 0) -> list:
     """
         upper_right_diagonals():
-            Extract upper-right diagonals going down-left,
+            Extract diagonals starting at the upper-right corner
+            and going down-right,
             i.e. start in the upper-right corner, moving down,
             extract diagonals starting at (0, k),
             for k = (n-1), (n-1), ... , 1. Since we want to use
@@ -67,34 +109,59 @@ def upper_right_diagonals(df: pd.DataFrame = DF,
             the transformed matrix, k = 0, 1, ... , n - 1, or
             rather, since we will be looking for the string 'XMAS',
             k = 4, ... , n - 1.
+
+    CORRECT
+
     """
-    # Convert to np.ndarray
+    # convert to np.ndarray
     d = np.asarray(df)
-    # then flip it left-right (equivalent to
-    # [r[::-1] for r in d]).
-    d = np.fliplr(d)
+    # then flip it left-right
+    # (equivalent to [r[::-1] for r in d]).
+    dflip = np.fliplr(d)
+    # then flip it upside-down
+    # (equivalent to dflip[::-1]);
+    # the upper-right corner is now
+    # at the bottom-left
+    dflipud = np.flipud(dflip)
+    # now, use lower_right_diagonals() to
+    # extract the diagonals (they will come
+    # out being reverse-sorted, but that's ok)
+    df_flipud = pd.DataFrame(dflipud)
+    # Amend start and stop positions (rows)
     if start == -1:
         start = 4
     if stop == 0:
         stop = len(df)
-    diags = []
-    for k in range(start, stop):
-        diag = np.fliplr(np.fliplr(d)[:k, :k]).diagonal()
-        diags.append(diag)
-    return diags
+    return lower_right_diagonals(df_flipud, N - start, N - stop)
 
 
-def upper_left_diagonals(df: pd.DataFrame = DF,
+def upper_left_diagonals(df = DF,
                          start=4,
                          stop=0) -> list:
     """
         upper-left_diagonals():
-            Extract upper-left diagonals going down-right,
+            Extract diagonals starting at the upper-left corner
+            and going down-left,
             i.e. start in the upper-left corner, moving down,
             extract diagonals starting at (0, k),
             for k = (n-1), (n-1), ... , 0.
+            i.e. start in the upper-right corner, moving down,
+            extract diagonals starting at (0, k),
+            for k = (n-1), (n-1), ... , 1. Since we want to use
+            np.diagonal(), which returns the diagonal starting
+            top-left and going down-right, we need to transform
+            the data by reversing the rows; for this we use
+            np.fliplr(), which is equivalent to
+            [row[::-1] for row in d]. Therefore, when processing
+            the transformed matrix, k = 0, 1, ... , n - 1, or
+            rather, since we will be looking for the string 'XMAS',
+            k = 4, ... , n - 1.
+
+    CORRECT!
+
     """
     d = np.asarray(df)
+    # Amend stop position (row)
     if stop == 0:
         stop = len(df)
     diags = []
@@ -104,32 +171,11 @@ def upper_left_diagonals(df: pd.DataFrame = DF,
     return diags
 
 
-def lower_right_diagonals(df: pd.DataFrame = DF,
-                          start:int = N - 4,
-                          stop: int = -1) -> list:
-    """
-        lower_right_diagonals():
-            Extract lower-right diagonals going down-right
-    """
-    d = np.asarray(df)
-    # Flip the array left-right so that we
-    # use of np.diagonal().
-    d = np.fliplr(d)
-    n = len(df)
-    if start == -1:
-        start = n - 4
-    diags = []
-    for k in range(start, stop, -1):
-        diag = d[k:n, :n-k].diagonal()
-        diags.append(diag)
-    return diags
-
-
 def extract_diagonals(df):
     """Extract diagonals from a dataframe"""
     diagonals = lower_left_diagonals(df, N - 1)
-    diagonals += upper_right_diagonals(df, 1)
     diagonals += upper_left_diagonals(df, 1)
+    diagonals += upper_right_diagonals(df, 1)
     diagonals += lower_right_diagonals(df, N - 1, -1)
     return diagonals
 
