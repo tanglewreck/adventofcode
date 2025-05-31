ed9ec6212be0130ce6b60cae0d3852e51d481ece
mier <mier@tanglewreck.mier.org>
2025-05-31 14:49:45 +0200

    helpers/day_4_count_matches.py: rewrote regexp stuff so that we're now ignoring case, whitespace, and '_', '.', ',', ' ' in the pattern matching

--- 2024/helpers/day_4_count_matches.py
+++ 2024/helpers/day_4_count_matches.py
@@ -14,6 +14,7 @@ import typing  # pylint: disable=unused-import
 from typing import Optional  # pylint: disable=unused-import
 from typing import List  # pylint: disable=unused-import
 from typing import Tuple  # pylint: disable=unused-import
+
 import numpy as np  # pylint: disable=unused-import
 import pandas as pd
 from . day_4_diags import extract_diagonals
@@ -28,34 +29,53 @@ def count_matches(df: pd.DataFrame,
         Search for a string embedded in the row, columns, and diagonals
         of a dataframe
     """
-    n = len(df)
-    regexp = re.compile(rf"{search_string}")
-    regexp_rev = re.compile(rf"{search_string[::-1]}")
+    n_rows = len(df)
+    n_cols = len(df.iloc[0])
+    if verbose:
+        print(f"n_rows = {n_rows}")
+        print(f"n_cols = {n_cols}")
+    if not search_string:
+        search_string = "xmas"
+    # Allow the data to contain '_', '.', ',', ' ',
+    # and ignore them in the search.
+    r = re.compile(r'[_., ]*'.join(re.split('', search_string)[1:-1]), re.IGNORECASE)
+    rr = re.compile(r'[_., ]*'.join(re.split('', search_string[::-1])[1:-1]), re.IGNORECASE)
+    # r = re.compile(r'x\s*m\s*a\s*s', re.IGNORECASE)
+    # rr = re.compile(r's\s*a\s*m\s*x', re.IGNORECASE)
+    #r = re.compile(r'x[_\s]*m[_\s]*a[_\s]*s', re.IGNORECASE)
+    #rr = re.compile(r's[_\s]*a[_\s]*m[_\s]*x', re.IGNORECASE)
     # Total number of hits
     n_matches_tot = 0
     # Search rows
     n_matches = 0
-    for row in range(n):
-        row_str = "".join(df.iloc[row])
-        n_matches += len(re.findall(regexp, row_str))
-        n_matches += len(re.findall(regexp_rev, row_str))
+    for row in range(n_rows):
+        row_str = "".join(df.iloc[row, 0:n_cols])
+        matches = re.findall(r, row_str)
+        matches_r = re.findall(rr, row_str)
+        n_matches += len(matches)
+        n_matches += len(matches_r)
+        if (matches or matches_r) and verbose > 1:
+            print(matches + matches_r)
+            row_str = row_str.replace("_", "")
+            print("count():", row_str.count("XMAS") + row_str.count("SAMX"))
+            print("count():", row_str.count("xmas") + row_str.count("samx"))
     n_matches_tot += n_matches
     if verbose:
         print(f"n_matches (rows) = {n_matches}")
     # Search columns
     n_matches = 0
-    for col in range(n):
+    for col in range(n_cols):
         col_str = "".join(df.iloc[:, col])
-        matches = re.findall(regexp, col_str)
-        matches_rev = re.findall(regexp_rev, col_str)
-        if matches and verbose > 1:
-            print(f"column {col}: ", end="")
-            print(matches)
-        if matches_rev and verbose > 1:
-            print(f"column {col}: ", end="")
-            print(matches_rev)
-        n_matches += len(re.findall(regexp, col_str))
-        n_matches += len(re.findall(regexp_rev, col_str))
+        matches = re.findall(r, col_str)
+        matches_r = re.findall(rr, col_str)
+        n_matches += len(matches)
+        n_matches += len(matches_r)
+        if (matches or matches_r) and verbose > 1:
+            print(f"column {col}: {col_str}")
+            print(matches + matches_r)
+            col_str = col_str.replace("_", "")
+            print("count():", col_str.count("XMAS") + col_str.count("SAMX"))
+            print("count():", col_str.count("xmas") + col_str.count("samx"))
     n_matches_tot += n_matches
     if verbose:
         print(f"n_matches (cols) = {n_matches}")
@@ -63,20 +83,18 @@ def count_matches(df: pd.DataFrame,
     n_matches = 0
     for k, diag in enumerate(extract_diagonals(df)):
         diag_str = "".join(diag)
-        diag_str_rev = "".join(diag[::-1])
+        matches = re.findall(r, diag_str)
+        matches_r = re.findall(rr, diag_str)
+        n_matches += len(matches)
+        n_matches += len(matches_r)
+        if (matches or matches_r) and verbose > 1:
+            print(f"{k}: diag {diag}: {diag_str}")
+            print(matches + matches_r)
+            diag_str = diag_str.replace("_", "")
+            print("count():", diag_str.count("XMAS") + diag_str.count("SAMX"))
+            print("count():", diag_str.count("xmas") + diag_str.count("samx"))
         if verbose > 1:
-            print(f"diag_str = {diag_str}")
-            print(f"diag_str_rev = {diag_str_rev}")
-        matches = re.findall(regexp, diag_str)
-        matches_rev = re.findall(regexp_rev, diag_str)
-        if matches and verbose > 1:
-            print(f"diag {k}: {diag}: ", end="")
-            print(matches)
-        if matches_rev and verbose > 1:
-            print(f"diag {k}: {diag}: ", end="")
-            print(matches_rev)
-        n_matches += len(re.findall(regexp, diag_str))
-        n_matches += len(re.findall(regexp, diag_str_rev))
+            print(f"{k}: diag_str = {diag_str}")
     n_matches_tot += n_matches
     if verbose:
         print(f"n_matches (diags) = {n_matches}")
