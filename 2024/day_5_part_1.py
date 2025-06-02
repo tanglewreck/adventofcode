ecf8b74d007fed66f7f2f2032f2e4ea741231531
mier <mier@tanglewreck.mier.org>
2025-06-02 11:52:06 +0200

    day_5_part_1.py, helpers/day_5_part_1.py: added code to read data from file: preliminary answer is 87 okay updates

--- 2024/day_5_part_1.py
+++ 2024/day_5_part_1.py
@@ -1,30 +1,30 @@
 """
 
     --- Day 5: Print Queue ---
-    
+
     Satisfied with their search on Ceres, the squadron of scholars suggests
     subsequently scanning the stationery stacks of sub-basement 17.
-    
+
     The North Pole printing department is busier than ever this close to
     Christmas, and while The Historians continue their search of this
     historically significant facility, an Elf operating a very familiar printer
     beckons you over.
-    
+
     The Elf must recognize you, because they waste no time explaining that the
     new sleigh launch safety manual updates won't print correctly. Failure to
     update the safety manuals would be dire indeed, so you offer your services.
-    
+
     Safety protocols clearly indicate that new pages for the safety manuals
     must be printed in a very specific order. The notation X|Y means that if
     both page number X and page number Y are to be produced as part of an
     update, page number X must be printed at some point before page number Y.
-    
+
     The Elf has for you both the page ordering rules and the pages to produce
     in each update (your puzzle input), but can't figure out whether each
     update has the pages in the right order.
-    
+
     For example:
-    
+
     47|53
     97|13
     97|61
@@ -46,31 +46,31 @@
     47|29
     75|13
     53|13
-    
+
     75,47,61,53,29
     97,61,53,29,13
     75,29,13
     75,97,47,61,53
     61,13,29
     97,13,75,29,47
-    
+
     The first section specifies the page ordering rules, one per line. The
     first rule, 47|53, means that if an update includes both page number 47
     and page number 53, then page number 47 must be printed at some point
     before page number 53. (47 doesn't necessarily need to be immediately
     before 53; other pages are allowed to be between them.)
-    
+
     The second section specifies the page numbers of each update. Because
     most safety manuals are different, the pages needed in the updates are
     different too. The first update, 75,47,61,53,29, means that the update
     consists of page numbers 75, 47, 61, 53, and 29.
-    
+
     To get the printers going as soon as possible, start by identifying
     which updates are already in the right order.
-    
+
     In the above example, the first update (75,47,61,53,29) is in the right
     order:
-    
+
         – 75 is correctly first because there are rules
         that put each other page after it: 75|47, 75|61,
         75|53, and 75|29.
@@ -78,37 +78,37 @@
         – 47 is correctly second because 75 must be before
         it (75|47) and every other page must be after it
         according to 47|61, 47|53, and 47|29.
-        
+
         – 61 is correctly in the middle because 75 and 47
         are before it (75|61 and 47|61) and 53 and 29 are
         after it (61|53 and 61|29).
-        
+
         – 53 is correctly fourth because it is before page
         number 29 (53|29).
-        
+
         – 29 is the only page left and so is
         correctly last.
-    
+
     Because the first update does not include some page numbers, the ordering
     rules involving those missing page numbers are ignored.
-    
+
     The second and third updates are also in the
     correct order according to the rules. Like the
     first update, they also do not include every page
     number, and so only some of the ordering rules
     apply - within each update, the ordering rules that
     involve missing page numbers are not used.
-    
+
     The fourth update, 75,97,47,61,53, is not in the
     correct order: it would print 75 before 97, which
     violates the rule 97|75.
-    
+
     The fifth update, 61,13,29, is also not in the
     correct order, since it breaks the rule 29|13.
-    
+
     The last update, 97,13,75,29,47, is not in the
     correct order due to breaking several rules.
-    
+
     For some reason, the Elves also need to know the
     middle page number of each update being printed.
     Because you are currently only printing the
@@ -116,55 +116,110 @@
     the middle page number of each correctly-ordered
     update. In the above example, the correctly-ordered
     updates are:
-    
+
     75,47,61,53,29
     97,61,53,29,13
     75,29,13
-    
+
     These have middle page numbers of 61, 53, and 29
     respectively. Adding these page numbers together
     gives 143.
-    
+
     Of course, you'll need to be careful: the actual
     list of page ordering rules is bigger and more
     complicated than the above example.
-    
+
     Determine which updates are already in the correct
     order.
 
     What do you get if you add up the middle page
     number from those correctly-ordered updates?
-    
+
     -->  <--
 
 """
 import os
+import pathlib
+from pathlib import Path
 import sys
 # from utils.as_dataframe import as_dataframe
 # from helpers.day_4_pt2 import x_mas_list, x_mas_pd, x_mas_np
 from helpers import __DATADIR__
+
+# from collections import defaultdict
+from helpers.day_5_part_1 import get_data
+# from helpers.day_5_part_1 import get_data, build_rules
+# from helpers.day_5_part_1 import UPDATES, RULES_EXAMPLE
 
 # Constants
 __NAME__ = "day_5"  # this is us
 __PART__ = "1"
 
 
-def main():
-    """main"""
+def get_input_file():
+    """get_input_file"""
+    #
+    # import argparse
+    #
     # Check for a commandline argument and use that as
     # the path to the file containing the input data
     try:
-        input_file = sys.argv[1]
+        input_file = Path(sys.argv[1])
         if not os.access(input_file, os.R_OK):
             raise OSError
     except OSError as exception:
         raise SystemExit("No such file") from exception
     except IndexError:
         # Default data input
-        input_file = f"{__DATADIR__}/{__NAME__}_part_{__PART__}_data"
+        input_file = pathlib.Path(
+                f"{__DATADIR__}/{__NAME__}_part_{__PART__}"
+        )
+    return input_file
+
+
+def main(verbose: int = 0):
+    """main"""
+    # Get name of input file
+    input_file = get_input_file()
     # Import data
-    # df = as_dataframe(input_file, save=False)
+    rules, updates = get_data(input_file)
+    # rules = build_rules()
+    # print(f"rules: {rules}")
+    all_ok = True
+    # n_updates = len(updates)
+    n_ok = 0
+    # for k, update in enumerate(UPDATES[3:n_updates+1]):
+    for k, update in enumerate(updates):
+        update_ok = True
+        if verbose > 0:
+            print(f"update[{k}]: {update}")
+        for i, u in enumerate(update):
+            n = len(update)
+            for j in update[i+1:n]:
+                ok = j in rules[u]
+                if verbose > 1:
+                    print(f"{j} in rules[{u}] = {ok}")
+                if not ok:
+                    all_ok = False
+                    update_ok = False
+        if update_ok:
+            n_ok += 1
+            print(f"update[{k}]: OK")
+        else:
+            print(f"update[{k}]: NOT ok")
+        # print()
+    empty_rules = False
+    for rulenum, rulelist in rules.items():
+        if not rulelist:
+            empty_rules = True
+            print(f"rule {rulenum} is empty ({rulelist})")
+    if empty_rules:
+        print("empty rules found")
+    else:
+        print("NO empty rules found")
+    print(f"all_ok = {all_ok}")
+    print(f"number of OK updates = {n_ok}")
 
 
 if __name__ == "__main__":
-    main()
+    main(verbose=0)
--- 2024/helpers/day_5_part_1.py
+++ 2024/helpers/day_5_part_1.py
@@ -1,19 +1,20 @@
 # coding: utf-8
 """check rules"""
 
+import pathlib
+from pathlib import Path, PosixPath
 from collections import defaultdict
-
 
 UPDATES = [
     [75, 47, 61, 53, 29],
     [97, 61, 53, 29, 13],
     [75, 29, 13],
-    [75,97,47, 61, 53],
+    [75, 97, 47, 61, 53],
     [61, 13, 29],
     [97, 13, 75, 29, 47]
 ]
 
-RULES_STR = """47:53
+RULES_EXAMPLE = """47:53
 97:13
 97:61
 97:47
@@ -36,10 +37,53 @@ RULES_STR = """47:53
 53:13
 """
 
+DEFAULT_DATA = pathlib.Path("data/day_5_part_1")
+
+
+def get_data(data_file: Path | PosixPath = DEFAULT_DATA) -> tuple[dict, list]:
+    """
+        get_data()
+            Read rules and updates from disk
+    """
+    with open(data_file) as fp:
+        # get rule
+        rules: dict[int, list[int]] = {}
+        pos = 0
+        while line := fp.readline().strip():
+            if "XX" in line:
+                break
+            pre, post = map(int, line.split("|"))
+    #        print(line, pre, post)
+            if pre in rules:
+                rules[pre].append(post)
+            else:
+                rules[pre] = [post]
+            pos += 1
+        pos += 1
+        # read updates
+        updates: list[list[int]] = []
+        while line := fp.readline().strip():
+            update = list(map(int, line.split(',')))
+            updates.append(update)
+            pos += 1
+        # Added rules for all pages that's in
+        # some update
+        for update in updates:
+            for page in update:
+                if page not in rules:
+                    rules[page] = []
+    # print(updates)
+    # [(k, " ".join([str(x) for x in v])) for k, v in rules.items()]
+    # print(len(rules))
+    return rules, updates
+
+
 def build_rules():
     """build rules"""
-    rules_list = RULES_STR.split("\n")
-    rules = defaultdict()
+    rules_list = RULES_EXAMPLE.split("\n")
+    # rules = defaultdict()
+    rules: dict[int, list] = {}
+    # rules = {}
     # make a rule for all values in UPDATES,
     # so we can check if there's a rule for
     # every update entry
@@ -61,28 +105,44 @@ def build_rules():
 
 def main():
     """main"""
-    rules = build_rules()
-    print(f"rules: {rules}")
+    verbose = 1
+    rules, updates = get_data()
+#     print("rules:")
+#     print(rules)
+#     print()
+#     print()
+#     print("updates")
+#     print(updates)
+#     for rulenum, rulelist in rules.items():
+#         if not rulelist:
+#             print(f"rule {rulenum} is empty ({rulelist})")
+    # rules = build_rules()
+    # print(f"rules: {rules}")
     all_ok = True
-    # n_updates = len(UPDATES)
+    # n_updates = len(updates)
+    n_ok = 0
     # for k, update in enumerate(UPDATES[3:n_updates+1]):
-    for k, update in enumerate(UPDATES):
+    for k, update in enumerate(updates):
         update_ok = True
-        print(f"update[{k}]: {update}")
+        if verbose > 0:
+            print(f"update[{k}]: {update}")
         for i, u in enumerate(update):
             n = len(update)
             for j in update[i+1:n]:
                 ok = j in rules[u]
-                print(f"{j} in rules[{u}] = {ok}")
+                if verbose > 1:
+                    print(f"{j} in rules[{u}] = {ok}")
                 if not ok:
                     all_ok = False
                     update_ok = False
         if update_ok:
+            n_ok += 1
             print(f"update[{k}]: OK")
         else:
             print(f"update[{k}]: NOT ok")
-        print()
+        # print()
     print(f"all_ok = {all_ok}")
+    print(f"number of OK updates = {n_ok}")
 
 
 if __name__ == "__main__":
