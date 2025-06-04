b30810f6ba55228be31b46678322a6608b4f0bad
mier <mier@tanglewreck.mier.org>
2025-06-04 20:02:32 +0200

    utils/__init__,py: added imports for all modules under utils/ + updated __all__

--- 2024/utils/__init__.py
+++ 2024/utils/__init__.py
@@ -5,6 +5,11 @@
 """
 
 import os
+from . as_dataframe import as_dataframe
+from . export_data import export_data
+from . import_data import import_data
+from . get_data_path import get_data_path
+from . verbose_msg import verbose_msg
 
 __version__ = "0.5.1"
 __author__ = "mier"
@@ -12,5 +17,12 @@ __name__ = "utils"  # pylint: disable=redefined-builtin
 
 __BASEDIR__ = f"{os.environ['HOME']}/Proj/adventofcode/2024"
 __DATADIR__ = f"{__BASEDIR__}/data"  # data directory path
+__PREFIX__ = "day"
 
-__all__ = ["__BASEDIR__", "__DATADIR__"]
+__all__ = ["__DATADIR__",
+           "__PREFIX__",
+           "as_dataframe",
+           "export_data",
+           "get_data_path",
+           "import_data",
+           "verbose_msg"]
